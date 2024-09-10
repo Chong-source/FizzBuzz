48460b2bf2dc09693aa9d5297002088e560fa16a
Chong <chong.wan@mail.utoronto.ca>
2024-09-10 14:03:41 -0400

    Added main class and called FizzBuzz in main

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+class Main{
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String[] newArgs = {"Hello", "from", "AnotherClass"};
+        fizzBuzz.main(newArgs);
+    }
+}
